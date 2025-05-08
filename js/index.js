99ecfd994380aede285a63aa4c6d5ce23475078c
jonathancode01 <icontatojonathan@gmail.com>
2025-05-08 08:20:47 -0400

    Atualização do título da tabela

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ document.getElementById('info-tabela').addEventListener('change', function () {
   
     if (mes === 'Janeiro') {
       const dados = [
-        { Produto: "Ar-Condicionado", Consumo: "120 kWh", Data: "2025-01-31" },
-        { Produto: "Equipamentos", Consumo: "30 m³", Data: "2025-01-31" },
-        { Produto: "Energia elétrica", Consumo: "110 kWh", Data: "2025-01-31" },
-        { Produto: "Água", Consumo: "28 m³", Data: "2025-01-28" },
-        { Produto: "Energia elétrica", Consumo: "130 kWh", Data: "2025-01-29" },
-        { Produto: "Água", Consumo: "29 m³", Data: "2025-01-30" },
+        { Equipamentos: "Ar-Condicionado", Consumo: "120 kWh", Data: "2025-01-31" },
+        { Equipamentos: "Equipamentos", Consumo: "30 m³", Data: "2025-01-31" },
+        { Equipamentos: "Energia elétrica", Consumo: "110 kWh", Data: "2025-01-31" },
+        { Equipamentos: "Água", Consumo: "28 m³", Data: "2025-01-28" },
+        { Equipamentos: "Energia elétrica", Consumo: "130 kWh", Data: "2025-01-29" },
+        { Equipamentos: "Água", Consumo: "29 m³", Data: "2025-01-30" },
 
       ];
   
@@ -20,7 +20,7 @@ document.getElementById('info-tabela').addEventListener('change', function () {
   
       dados.forEach(item => {
         html += `<tr>
-          <td>${item.Produto}</td>
+          <td>${item.Equipamentos}</td>
           <td>${item.Consumo}</td>
           <td>${item.Data}</td>
         </tr>`;
